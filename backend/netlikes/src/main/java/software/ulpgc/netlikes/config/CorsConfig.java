8199764ecaa27f0b856aca485c27a434a89eff96
jose <josemiguelojedahernandez77@gmail.com>
2026-05-01 00:08:15 +0000

    chore(backend): update securityconfig and corsconfig

--- backend/netlikes/src/main/java/software/ulpgc/netlikes/config/CorsConfig.java
+++ backend/netlikes/src/main/java/software/ulpgc/netlikes/config/CorsConfig.java
@@ -9,11 +9,10 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
+        registry.addMapping("/*")
+                .allowedOrigins("https://net-likes.vercel.app/", "http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("")
                 .allowCredentials(true);
     }
-    
 }
--- backend/netlikes/src/main/java/software/ulpgc/netlikes/config/SecurityConfig.java
+++ backend/netlikes/src/main/java/software/ulpgc/netlikes/config/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.Customizer;
+
 
 @Configuration
 @EnableWebSecurity
@@ -13,6 +15,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+            .cors(Customizer.withDefaults())
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
                 .anyRequest().permitAll()
