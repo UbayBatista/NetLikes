ff04bb007079754d906eb799ac0b901aef9225c0
AliciaM05 <aliciarotru@gmail.com>
2026-05-03 12:17:32 +0100

    fix(backend): missing }

--- backend/netlikes/src/main/java/software/ulpgc/netlikes/controller/FollowController.java
+++ backend/netlikes/src/main/java/software/ulpgc/netlikes/controller/FollowController.java
@@ -77,6 +77,8 @@ public class FollowController {
     @DeleteMapping("/{followerId}/reject")
     public ResponseEntity<Void> rejectFollow(@PathVariable String followerId, @RequestHeader("X-User-Id") String myId) {
         followService.rejectFollow(followerId, myId);
+    }
+    
     @DeleteMapping("/{targetId}/remove")
     public ResponseEntity<Void> removeFollower(
             @PathVariable String targetId, 
